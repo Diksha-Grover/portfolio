d62aaaf4d6128dc1a07dd7d2c8f463ff86f061c4
Diksha-Grover <thedikshagrover@gmail.com>
2025-08-08 12:28:45 +0530

    projects section

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,91 @@
+"use client";
+import React, { useState, useTransition } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Javascript</li>
+        <li>Typescript</li>
+        <li>React.js</li>
+        <li>Next.js</li>
+        <li>Deno</li>
+        <li>Remix.run</li>
+        <li>SQL</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Bachelor of Engineering(B.E.) in Computer Science, Maharashtra</li>
+      </ul>
+    ),
+  },
+  // {
+  //   title: "Certifications",
+  //   id: "certifications",
+  //   content: (
+  //     <ul className="list-disc pl-2">
+  //       <li>AWS Cloud Practitioner</li>
+  //     </ul>
+  //   ),
+  // },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white">
+      <div className="gap-8 items-center py-8 px-4 xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} alt='' />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-white texxt-base md:text-lg">
+            I am a Frontend Developer with over 4 years of professional experience in building responsive and user-friendly web applications.
+            I’ve worked on a variety of domains like logistics, healthcare, insurance, team management, banking and financial services, gaining valuable hands-on experience in real-world projects.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              Skills
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              Education
+            </TabButton>
+            {/* <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              Certifications
+            </TabButton> */}
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ src/app/components/ProjectSection.jsx
@@ -0,0 +1,71 @@
+"use client";
+import React from "react";
+import ProjectCard from "./ProjectCard";
+
+const projectsData = [
+  {
+    id: 1,
+    title: "Netflix Clone Website",
+    description: "Built with React, features movie listings, routing, and a responsive UI design.",
+    image: "/images/projects/1.png",
+    link: "https://diksha-grover.github.io/NetflixClone/",
+  },
+  {
+    id: 2,
+    title: "Bankist Wesite",
+    description: "Banking UI app with DOM manipulation, smooth scrolling, and interactive elements.",
+    image: "/images/projects/2.png",
+    link: "https://diksha-grover.github.io/bankist-javascript/",
+  },
+  {
+    id: 3,
+    title: "Stripe Submenus",
+    description: "Dynamic submenus with React and Context API, inspired by Stripe’s navigation.",
+    image: "/images/projects/3.png",
+    link: "https://13-stripe-submenus-react.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "Food Ordering Application",
+    description: "React-based food app with filtering, cart features, and clean responsive layout.",
+    image: "/images/projects/4.png",
+    link: "https://15-cocktail-react.vercel.app/",
+  },
+  {
+    id: 5,
+    title: "Mapty Wesite",
+    description: "Workout tracker using Geolocation, Leaflet, and local storage for data handling.",
+    image: "/images/projects/5.png",
+    link: "https://diksha-grover.github.io/mapty-js/",
+  },
+  {
+    id: 6,
+    title: "E-commerce Application",
+    description: "Product listing, cart, auth, and CRUD built with React and Firebase backend.",
+    image: "/images/projects/6.png",
+    link: "https://shopping-cart-sigma-two.vercel.app/",
+  },
+];
+const ProjectsSection = () => {
+
+  return (
+    <>
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        My Projects
+      </h2>
+      <div className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {projectsData.map((project, index) => (
+          <ProjectCard
+            key={project.id}
+            title={project.title}
+            description={project.description}
+            imgUrl={project.image}
+            link={project.link}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default ProjectsSection;
