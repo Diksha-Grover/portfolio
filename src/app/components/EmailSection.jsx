b281479099cc027781128ebcdefa1ddc5e94cae2
Diksha-Grover <thedikshagrover@gmail.com>
2025-08-13 15:10:23 +0530

    email submission

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -64,68 +64,72 @@ const EmailSection = () => {
             </div>
             <div>
                 {/* <form ref={formRef} className="flex flex-col" onSubmit={handleSubmit}> */}
-                    <form ref={formRef} className="flex flex-col">
-                        <div className="mb-6">
-                            <label
-                                htmlFor="email"
-                                className="text-white block mb-2 text-sm font-medium"
-                            >
-                                Your email
-                            </label>
-                            <input
-                                name="email"
-                                type="email"
-                                id="email"
-                                required
-                                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                                placeholder="john@google.com"
-                            />
-                        </div>
-                        <div className="mb-6">
-                            <label
-                                htmlFor="subject"
-                                className="text-white block text-sm mb-2 font-medium"
-                            >
-                                Subject
-                            </label>
-                            <input
-                                name="subject"
-                                type="text"
-                                id="subject"
-                                required
-                                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                                placeholder="Just saying hi"
-                            />
-                        </div>
-                        <div className="mb-6">
-                            <label
-                                htmlFor="message"
-                                className="text-white block text-sm mb-2 font-medium"
-                            >
-                                Message
-                            </label>
-                            <textarea
-                                name="message"
-                                id="message"
-                                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                                placeholder="Let's talk about..."
-                            />
-                        </div>
-                        <div className="mb-6">
-                            <button
-                                type="submit"
-                                className="bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
-                            >
-                                {" "}
-                                Send message{" "}
-                            </button>
-                        </div>
-                    </form>
-                    {emailSubmitted && (
-                        <p className="text-green-500 text-sm mt-2">
-                            Email sent successfully!
-                        </p>
-                    )}
+                <form ref={formRef} className="flex flex-col">
+                    <div className="mb-6">
+                        <label
+                            htmlFor="email"
+                            className="text-white block mb-2 text-sm font-medium"
+                        >
+                            Your email
+                        </label>
+                        <input
+                            name="email"
+                            type="email"
+                            id="email"
+                            required
+                            className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                            placeholder="john@google.com"
+                        />
+                    </div>
+                    <div className="mb-6">
+                        <label
+                            htmlFor="subject"
+                            className="text-white block text-sm mb-2 font-medium"
+                        >
+                            Subject
+                        </label>
+                        <input
+                            name="subject"
+                            type="text"
+                            id="subject"
+                            required
+                            className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                            placeholder="Just saying hi"
+                        />
+                    </div>
+                    <div className="mb-6">
+                        <label
+                            htmlFor="message"
+                            className="text-white block text-sm mb-2 font-medium"
+                        >
+                            Message
+                        </label>
+                        <textarea
+                            name="message"
+                            id="message"
+                            className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                            placeholder="Let's talk about..."
+                        />
+                    </div>
+                    <div className="mb-6">
+                        <button
+                            type="submit"
+                            className="bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
+                            onClick={() => {
+                                setEmailSubmitted(true)
+                                formRef.current.reset();
+                            }}
+                        >
+                            {" "}
+                            Send message{" "}
+                        </button>
+                    </div>
+                </form>
+                {emailSubmitted && (
+                    <p className="text-green-500 text-sm mt-2">
+                        Email sent successfully!
+                    </p>
+                )}
             </div>
         </section>
     );
